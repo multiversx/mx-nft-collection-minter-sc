81e9d457282909fb63ebfe7c3a065dd88a495f5b
Alin Cruceat <alyn_509@yahoo.com>
2024-05-09 14:00:56 +0300

    fix scenario tests 2

--- esdt-system-sc-mock/tests/esdt_system_sc_mock_rs_test.rs
+++ esdt-system-sc-mock/tests/esdt_system_sc_mock_rs_test.rs
@@ -4,15 +4,10 @@ fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
     blockchain.set_current_dir_from_workspace("esdt-system-sc-mock");
     blockchain.register_contract(
-        "file:output/esdt-system-sc-mock.wasm",
+        "mxsc:output/esdt-system-sc-mock.mxsc.json",
         esdt_system_sc_mock::ContractBuilder,
     );
     blockchain
-}
-
-#[test]
-fn adder_rs() {
-    world().run("scenarios/adder.scen.json");
 }
 
 #[test]
