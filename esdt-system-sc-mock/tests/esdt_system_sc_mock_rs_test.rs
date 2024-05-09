73fb723a1939c7a04f1b06132da54e2992733d90
Alin Cruceat <alyn_509@yahoo.com>
2024-05-09 14:52:42 +0300

    scenario cleanup

--- esdt-system-sc-mock/tests/esdt_system_sc_mock_rs_test.rs
+++ esdt-system-sc-mock/tests/esdt_system_sc_mock_rs_test.rs
@@ -2,7 +2,7 @@ use multiversx_sc_scenario::*;
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
-    blockchain.set_current_dir_from_workspace("esdt-system-sc-mock");
+//    blockchain.set_current_dir_from_workspace("esdt-system-sc-mock");
     blockchain.register_contract(
         "mxsc:output/esdt-system-sc-mock.mxsc.json",
         esdt_system_sc_mock::ContractBuilder,
